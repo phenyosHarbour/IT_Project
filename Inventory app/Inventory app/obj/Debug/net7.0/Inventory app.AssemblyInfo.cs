bf99bd8b7ce8b6bdbd237c03d62be52c637dc274
phenyosHarbour <phenyosharbour@gmail.com>
2024-10-30 17:19:05 +0200

    Inititial Project Structure

--- Inventory app/Inventory app/obj/Debug/net7.0/Inventory app.AssemblyInfo.cs
+++ Inventory app/Inventory app/obj/Debug/net7.0/Inventory app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b5f1d99baf1f8f15aa7249713bac693ae5f7ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d72fe4830850ed8b7a8fe702843feb776a5a623")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
